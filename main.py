f378b04a6be24451b9a541955dffd155345d1ef0
AadithiA <148992895+AadithiA@users.noreply.github.com>
2023-10-31 12:09:34 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 # Aadithi Arjun
-def menu():
+def main():
+  while True:
     print("Menu")
     print("-------------")
     print("1. Encode")
     print("2. Decode")
     print("3. Quit")
-
-
-def main():
-    print(menu())
     option = input("Please enter an option:")
-    print(option)
-    if option=='1':
-        UserChoice1 = input("Please enter your password to encode:")
-        print(UserChoice1)
-        print("Your password has been encoded and stored!")
-    elif option =='2':
-        print("a")
-    elif option =='3':
-        break
+
+    if option == '1':
+      global UserChoice1
+      UserChoice1 = int(input("Please enter your password to encode:"))
+      print(UserChoice1)
+      print("Your password has been encoded and stored!")
+
+    elif option == '2':
+      encoded_password = ''
+      for digit in str(UserChoice1):
+        new_digit = int(digit) + 3
+        encoded_password += str(new_digit)
+      print("The encoded password is", encoded_password, "and the original password is", UserChoice1)
+
+    elif option == '3':
+      False
+
 if __name__ == "__main__":
     main()
-
-
-
